e7ee36a36d658e8c432defe8496f36ad125dd996
Jaime Oakes <oakesjaime@gmail.com>
2024-12-01 21:59:38 -0600

    mudancas em todos os projetos. Adicionando novo projeto todo list ao portfolio

---
+++ scripts/todo-list.js
@@ -0,0 +1,63 @@
+// Array inicial de tarefas
+const todoList = [
+  {
+    name: 'make dinner',
+    dueDate: '2024-12-22'
+  },
+  {
+    name: 'wash dishes',
+    dueDate: '2024-12-22'
+  },
+  {
+    name: 'Walk the dogs',
+    dueDate: '2024-12-22'
+  },
+  {
+    name: 'take a shower',
+    dueDate: '2024-12-22'
+  }
+];
+
+// Renderiza a lista de tarefas na tela
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const { name, dueDate } = todoList[i];
+    const html = `
+      <div>${name}</div> 
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+// Adiciona uma nova tarefa à lista
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  // Validação para evitar campos vazios
+  if (name === '' || dueDate === '') {
+    alert('Please fill out both fields');
+    return;
+  }
+
+  todoList.push({ name, dueDate }); // Adiciona a nova tarefa ao array
+
+  inputElement.value = ''; // Limpa o campo de entrada de texto
+  dateInputElement.value = ''; // Limpa o campo de data
+
+  renderTodoList(); // Re-renderiza a lista de tarefas
+}
